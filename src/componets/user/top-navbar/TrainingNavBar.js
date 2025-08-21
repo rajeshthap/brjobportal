1103025ad438a15a0a9f987407004cef2e4af6b9
Deepika <deepikachauhan8988@gmail.com>
2025-08-21 20:45:23 +0530

    navigation bar contain changes

--- src/componets/user/top-navbar/TrainingNavBar.js
+++ src/componets/user/top-navbar/TrainingNavBar.js
@@ -99,36 +99,13 @@ function TrainingNavBar() {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto nav-list gap-4 justify-content-end flex-grow-1 d-flex nav-down">
               {/* Training Dropdown */}
-              <NavDropdown title="Training" id="training-dropdown">
-                <NavDropdown.Item as={Link} to="/TrainingReact" onClick={handleNavClick}>React JS</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/TrainingPython" onClick={handleNavClick}>Python</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/TrainingPHP" onClick={handleNavClick}>PHP</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/TrainingMySql" onClick={handleNavClick}>MySQL</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/TrainingBootstrap" onClick={handleNavClick}>HTML/CSS/Bootstrap</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/TrainingWebDesign" onClick={handleNavClick}>Web Development</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/UIUXTraining" onClick={handleNavClick}>UI/UX Designer</NavDropdown.Item>
-              </NavDropdown>
-
-              {/* Grooming Class Dropdown */}
-              <NavDropdown title="Grooming Class" id="grooming-dropdown">
-                <NavDropdown.Item as={Link} to="/Communication" onClick={handleNavClick}>Communication Skills</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/SelfConfidence" onClick={handleNavClick}>Self-Confidence & Power Dressing</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/InterviewSkill" onClick={handleNavClick}>Interview Skills</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/PublicSpeaking" onClick={handleNavClick}>Public Speaking</NavDropdown.Item>
-              </NavDropdown>
-
-              {/* Study Material */}
-              <NavDropdown title="Study Material" id="study-material-dropdown">
-                <NavDropdown.Item as={Link} to="/PdfTutorial" onClick={handleNavClick}>PDF Tutorial</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/VideoTutorial" onClick={handleNavClick}>Video Tutorial</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/LiveClass" onClick={handleNavClick}>Live Class</NavDropdown.Item>
-              </NavDropdown>
-
+             <Nav.Link as={Link} to="/Grooming Class" onClick={handleNavClick}>Grooming Class</Nav.Link>
+      <Nav.Link as={Link} to="/Training" onClick={handleNavClick}>Training</Nav.Link>
+      <Nav.Link as={Link} to="/GroomingClass" onClick={handleNavClick}>Grooming Class</Nav.Link>
+      <Nav.Link as={Link} to="/StudyMaterial" onClick={handleNavClick}>Study Material</Nav.Link>
+      <Nav.Link as={Link} to="/Event" onClick={handleNavClick}>Event</Nav.Link>
               {/* Events */}
-              <NavDropdown title="Event" id="event-dropdown">
-                <NavDropdown.Item as={Link} to="/Event" onClick={handleNavClick}>Upcoming Events</NavDropdown.Item>
-              </NavDropdown>
-
+             
               {/* Login Buttons */}
               {!isLoggedIn && (
                 <>
